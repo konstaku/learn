f541a12aae5061f310671747fa0fa2581bbe7c59
konstantinkupriyanov <0864380@gmail.com>
2023-02-07 20:45:05 +0100

    another day another commit

--- async/work-in-progress.js
+++ async/work-in-progress.js
@@ -1,19 +1,25 @@
 'use strict'
-class FormatError extends SyntaxError {
-  constructor(desc) {
-    super(desc);
-    this.name = this.constructor.name;
+
+let generator = function(num) {
+  return num * 16807 % 2147483647;
+}
+
+const generatorDecorator = function(func) {
+  let current = 0;
+  let next = 0;
+
+  return function(x) {
+    current = x;
+    next = func(current);
+    current += next;
+    return next;
   }
 }
 
-// making some edits
+generator = generatorDecorator(generator);
 
-let err = new FormatError('Oshibka formatirovaniya');
-
-console.log(err.message);
-console.log(err.name);
-console.log(err.stack);
-
-console.log(err instanceof FormatError);
-console.log(err instanceof SyntaxError);
-
+console.log(generator(1));
+console.log(generator(1));
+console.log(generator(1));
+console.log(generator(1));
+console.log(generator(1));
