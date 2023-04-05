fbc8da059801bc2312aacde58a2ed902f6b2327f
Konstantin Kupriyanov <0864380@gmail.com>
2023-04-05 21:20:12 +0200

    cheat commit

---
+++ YDKJS/iterator/this.js
@@ -0,0 +1,24 @@
+function classroom(teacher) {
+  return function study() {
+    console.log(`
+      ${ teacher } says to study ${ this.topic }
+    `);
+  };
+}
+
+var assignment = classroom('Kyle');
+
+assignment();
+
+var homework = {
+  topic: 'JS',
+  assignment: assignment,
+};
+
+homework.assignment();
+
+var otherHomework = {
+  topic: 'Math',
+};
+
+assignment.call(otherHomework);
