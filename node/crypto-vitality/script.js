9ed28919445f861024241d3319a6103b46bdfdf6
Konstantin Kupriyanov <0864380@gmail.com>
2023-04-30 17:41:45 +0200

    Updated to work on dune database instead of graph

--- node/crypto-vitality/script.js
+++ node/crypto-vitality/script.js
@@ -1,11 +1,16 @@
 const uniPoolListOptimism = require ('./data/weth_pools_uni_v3_optimism.json');
 const uniPoolListArbitrum = require ('./data/weth_pools_uni_v3_arbitrum.json');
 
+const uniPoolsAndFeesOptimism = require ('./data/uni_v3_optimism_pools_and_fees.json');
+const uniPoolsAndFeesPolygon = require ('./data/uni_v3_polygon_pools_and_fees.json');
+
+const DUNE_API_KEY = 'o0T6Pl9KPv2fjRgqnhDegftHX5RSbg2z';
+
 // The query limit is max of 30 pools at once, so the function forms batches 
 // of 30 addresses (or a remaining amount of addresses), and calls the loadBatch 
 // function with the current batch until all pools are called.
 async function fetchHotPools(pools, network) {
-  const entryPoolData = pools.data.liquidityPools;
+  const entryPoolData = pools.result.rows;
   const resultingPoolData = [];
 
   let poolChunk = [];
@@ -16,12 +21,13 @@ async function fetchHotPools(pools, network) {
     i++;
 
     if (poolChunk.length == 30) {
-      console.log(`Loading pool chunk, currently ${resultingPoolData.length} elements in result array`);
+      console.log(`Loading pool chunk, ${i} pools loaded`);
       await loadBatch(poolChunk, resultingPoolData, network);
       poolChunk = [];
     }
 
     if (i == entryPoolData.length - 1) {
+      console.log(`Loading final chunk, ${i} pools loaded`);
       await loadBatch(poolChunk, resultingPoolData, network)
       poolChunk = null;
       break;
@@ -32,45 +38,46 @@ async function fetchHotPools(pools, network) {
 }
 
 async function loadBatch(chunk, base, network) {
-  const urls = chunk.map(el => fetch(`https://api.dexscreener.com/latest/dex/pairs/${network}/${el.id}`));
+  const urls = chunk.map(el => fetch(`https://api.dexscreener.com/latest/dex/pairs/${network}/${el.address}`));
 
   await Promise.all(urls) // 1: Add await
-    .then(responses => {
-      return Promise.all(responses.map(el => el.json()));
-    }) // 2: *Return* Promise all, not just execute
+    .then(responses => Promise.all(responses.map(el => el.json()))) // 2: *Return* Promise all, not just execute
     .then(results => { 
       for (let i = 0; i < results.length; i++) {
         
-        if (!results[i].pair) {
+        if (!results[i].pair || !results[i].pair.liquidity) {
+          continue;
+        }
+
+        if (results[i].pair.liquidity.usd < 1000) {
           continue;
         }
 
         const pairInfo = {
           pair: `${results[i].pair.baseToken.name}/${results[i].pair.quoteToken.name}`,
           vitality: null,
-          tvl: results[i].pair.liquidity ? results[i].pair.liquidity.usd : null,
+          tvl: results[i].pair.liquidity.usd,
           volume: results[i].pair.volume.h24,
-          fee: +chunk[i].fees[2].feePercentage,
+          fee: chunk[i].fees / 10000,
           address: results[i].pair.pairAddress,
         };
 
-        // IMPORTANT to add || 0, otherwise sort will not work properly
+        // IMPORTANT to add || null, otherwise sort will not work properly
         pairInfo.vitality = pairInfo.volume / pairInfo.tvl * pairInfo.fee || null;
         base.push(pairInfo);
       }
 
-      console.log('base', base);
       return base;
     })
     .then(result => result.sort((a, b) => b.vitality - a.vitality))
     .catch(e => console.log('Error:', e));
 }
 
-fetchHotPools(uniPoolListArbitrum, 'arbitrum')
+fetchHotPools(uniPoolsAndFeesPolygon, 'polygon')
   .then(results => {
-    console.log('10 hottest arbitrum pools:');
+    console.log('10 hottest polygon pools:');
 
-    for (let i = 0; i < results.length; i++) {
-      console.log('\x1b[36m%s\x1b[0m', 'Pool:', results[i].pair, 'TVL:', results[i].tvl, 'vitality:', results[i].vitality);
+    for (let i = 0; i < 10; i++) {
+      console.log(`Pool: ${results[i].pair}  TVL: ${results[i].tvl}   vitality: ${results[i].vitality}  Address: ${results[i].address}`);
     }
   });
