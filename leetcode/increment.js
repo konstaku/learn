24ea243d6aaf7613be7bfd6399dd2aa724021d52
Konstantin Kupriyanov <0864380@gmail.com>
2023-06-22 22:00:58 +0200

    leetcode

---
+++ leetcode/increment.js
@@ -0,0 +1,23 @@
+function inc(arr) {
+        if (arr[arr.length-1] === 9) {
+                move(arr, arr.length-1);
+        } else {
+                arr[arr.length-1] += 1;
+        }
+
+        function move(array, index) {
+                array[index] = 0;
+                if (array[index-1] > 0 && array[index-1] < 9) {
+                        array[index-1]++;
+                } else if (array[index-1] === 9) {
+                        move(array, index-1);
+                } else {
+                        array.unshift(1);
+                }
+                return array;
+        }
+
+        return arr;
+}
+
+console.log(inc([2, 3, 9]));
