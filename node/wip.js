de28c9851b0a41ec87f7e3118cf0be6cb5128380
Konstantin Kupriyanov <0864380@gmail.com>
2023-07-24 14:18:19 +0200

    linked list

--- node/wip.js
+++ node/wip.js
@@ -1,7 +1,21 @@
-const o = [];
+'use strict';
+import { Node, LinkedList } from './linkedList.js';
 
-o.__proto__ = { 0: 'a',  1: 'b', 2: 'c' };
+const node1 = new Node('Once');
+const node2 = new Node('Upon');
+const node3 = new Node('A');
+const node4 = new Node('Time');
 
-for (const i in o) {
-    console.log(o[i])
-}
+const list = new LinkedList(node1);
+
+node1.nextNode = node2;
+node2.nextNode = node3;
+node3.nextNode = node4;
+
+list.insertAtIndex(0, '********');
+list.deleteAtIndex(4);
+
+console.log(list.read(0).data);
+console.log(list.read(1).data);
+console.log(list.read(2).data);
+console.log(list.read(3).data);
