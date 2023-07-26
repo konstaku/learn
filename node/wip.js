35b9c43fe53f95b49b531053487c3731774332e6
Konstantin Kupriyanov <0864380@gmail.com>
2023-07-26 22:16:18 +0200

    commit from neovim

--- node/wip.js
+++ node/wip.js
@@ -1,21 +1,12 @@
-'use strict';
-import { Node, LinkedList } from './linkedList.js';
+import { BinaryNode } from './BinaryTree.js';
+const { log } = console;
 
-const node1 = new Node('Once');
-const node2 = new Node('Upon');
-const node3 = new Node('A');
-const node4 = new Node('Time');
+const root = new BinaryNode('Once');
+root.insert('Upon');
+root.insert('Z');
+root.insert('A');
+root.insert('Time');
+root.insert('Yankee');
 
-const list = new LinkedList(node1);
+log(root);
 
-node1.nextNode = node2;
-node2.nextNode = node3;
-node3.nextNode = node4;
-
-list.insertAtIndex(0, '********');
-list.deleteAtIndex(4);
-
-console.log(list.read(0).data);
-console.log(list.read(1).data);
-console.log(list.read(2).data);
-console.log(list.read(3).data);
