b56457ee04e12f3462ff5e076466ff905eaf34b4
Konstantin Kupriyanov <0864380@gmail.com>
2023-07-28 13:07:43 +0200

    Binary tree deletion implemented

--- node/BinaryTree.js
+++ node/BinaryTree.js
@@ -1,74 +1,117 @@
+export class BinaryTree {
+    constructor(values) {
+        const root = new BinaryNode(values[0]);
+
+        for (let i = 1; i < values.length; i++) {
+            values[i] = root.insertNode(values[i]);
+        }
+
+        return root;
+    }
+}
+
 export class BinaryNode {
-    constructor(value, left = null, right = null) {
+    constructor(value, left = null, right = null) {1
         this.value = value;
         this.left = left;
         this.right = right;
     }
 
-    search(value, node = this) {
+    insertNode(value, node = this) {
+        if (this.value === null) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+
+        else if (value < node.value) {
+            if (node.left === null) {
+                node.left = new BinaryNode(value);
+            } 
+
+            else {
+                this.insertNode(value, node.left);
+            }
+        } 
+
+        else if (value > node.value) {
+            if (node.right === null) {
+                node.right = new BinaryNode(value);
+            } 
+
+            else {
+                this.insertNode(value, node.right);
+            }
+        }
+    }
+
+    searchNode(value, node = this) {
         if (node === null || node.value === value) {
             return node;
-        } else if (value < node.value) {
-            return this.search(value, node.left);
-        } else if (value > node.value) {
-            return this.search(value, node.right);
-        } else {
+        } 
+
+        else if (value < node.value) {
+            return this.searchNode(value, node.left);
+        } 
+
+        else if (value > node.value) {
+            return this.searchNode(value, node.right);
+        } 
+
+        else {
             return null;
         }
     }
 
-    insert(value, node = this) {
-        if (value < node.value) {
-            if (node.left === null) {
-                node.left = new BinaryNode(value);
-            } else {
-                this.insert(value, node.left);
-            }
-        } else if (value > node.value) {
-            if (node.right === null) {
-                node.right = new BinaryNode(value);
-            } else {
-                this.insert(value, node.right);
-            }
-        }
-    }
-
-    delete(valueToDelete, node = this) {
+    deleteNode(valueToDelete, node = this) {
         if (node === null) {
             return null;
         }
 
         else if (valueToDelete < node.value) {
-            nodee.left = this.delete(valueToDelete, node.left);
+            node.left = this.deleteNode(valueToDelete, node.left);
             return node;
         }
 
         else if (valueToDelete > node.value) {
-            node.right = this.delete(valueToDelete, node.right);
+            node.right = this.deleteNode(valueToDelete, node.right);
             return node;
         }
 
+        // We are deep enough to find the node to delete:
         else if (valueToDelete === node.value) {
+            // If no left child, return right child
             if (node.left === null) {
                 return node.right;
             }
 
+            // If there is a left child, but no right child, return left child
             else if (node.right === null) {
                 return node.left;
             }
 
+            // If the node has both childs
             else {
-                node.right = this.lift(node.right, node);
+                // Right child of a node is now the right child of the leftmost node
+                node.right = this.liftNode(node.right, node);
                 return node;
             }
         }
     }
 
-    lift(node, nodeToDelete) {
+    // Function is called in case when the deleted node has both childs
+    // Its purpose is to find leftmost value of all childs and put it on top
+    liftNode(node, nodeToDelete) {
+        // Recursively look for a leftmost child
         if (node.left !== null) {
-            node.left = this.lift(node.left, nodeToDelete);
+            node.left = this.liftNode(node.left, nodeToDelete);
             return node;
-        } else {
+        } 
+
+        // Base case: we found the leftmost child
+        // Putting leftmost child value as a value of node to delete,
+        // returning its right child with its branch
+        else {
             nodeToDelete.value = node.value;
             return node.right;
         }
--- node/wip.js
+++ node/wip.js
@@ -1,15 +1,23 @@
-import { BinaryNode } from './BinaryTree.js';
-const { log } = console;
+import { BinaryNode, BinaryTree } from './BinaryTree.js';
 
-const root = new BinaryNode('Once');
-root.insert('Upon');
-root.insert('Z');
-root.insert('A');
-root.insert('Time');
-root.insert('Yankee');
+// Create an array of 1000 numbers
+const numbersArray = new Array(1000000);
+for (let i = 0; i < 1000000; i++) {
+    numbersArray[i] = i;
+}
 
-log('tree before deletion:', root);
+// Shuffle an array
+for (let i = 0; i < 100000000; i++) {
+    const x = Math.floor(Math.random() * 1000000);
+    const y = Math.floor(Math.random() * 1000000);
 
-root.delete('Z');
+    const temp = numbersArray[x];
+    numbersArray[x] = numbersArray[y];
+    numbersArray[y] = temp;
+}
 
-log('tree after deletion:', root);
+console.log(numbersArray);
+
+const tree = new BinaryTree(numbersArray);
+
+console.log(tree);
