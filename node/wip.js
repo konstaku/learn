020e1c0e8b99addc9f816696c41b84619d02c11d
Konstantin Kupriyanov <0864380@gmail.com>
2023-08-10 12:53:40 +0200

    useArray

--- node/wip.js
+++ node/wip.js
@@ -1,17 +1,7 @@
-import { City } from './Graph.js';
+const a = 0;
 
-const atlanta = new City('Atlanta');
-const boston = new City('Boston');
-const chicago = new City('Chicago');
-const denver = new City('Denver');
-const elPaso = new City('El Paso');
+const obj = {
+    foo: a == 0 ? 5 : 10,
+}
 
-atlanta.addRoute(boston, 100)
-atlanta.addRoute(denver, 160)
-boston.addRoute(chicago, 120)
-boston.addRoute(denver, 180)
-chicago.addRoute(elPaso, 80)
-denver.addRoute(chicago, 40)
-denver.addRoute(elPaso, 140)
-
-console.log(atlanta.dijkstra(atlanta, [boston, chicago, denver, elPaso]));
+console.log(obj);
