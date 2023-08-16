34d4c0545411742fe516dd4801b3beb296a5fcc0
Konstantin Kupriyanov <0864380@gmail.com>
2023-08-16 14:20:31 +0200

    css layout portfolio project

--- react/basic-form-validation/src/App.jsx
+++ react/basic-form-validation/src/App.jsx
@@ -1,86 +1,10 @@
 import { useRef, useState } from 'react';
 import './styles.css';
+import { FormValidationRef } from './FormValidationRef';
+import { FormValidationState } from './FormValidationState';
 
 function App() {
-  const nameRef = useRef();
-  const passwordRef = useRef();
-
-  const [passwordFormClass, setPasswordFormClass] = useState('form-group');
-  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
-  const [emailFormClass, setEmailFormClass] = useState('form-group');
-  const [emailErrorMessage, setEmailErrorMessage] = useState('');
-
-  return (
-    <>
-      <form className="form" onSubmit={handleSubmit}>
-        <div className={emailFormClass}>
-          <label htmlFor="email" className="label">
-            Email
-          </label>
-          <input
-            className="input"
-            type="email"
-            id="email"
-            ref={nameRef}
-            required
-          />
-          <div className="msg">{emailErrorMessage}</div>
-        </div>
-        <div className={passwordFormClass}>
-          <label htmlFor="password" className="label">
-            Password
-          </label>
-          <input
-            id="password"
-            className="input"
-            type="password"
-            ref={passwordRef}
-            required
-          />
-          <div className="msg">{passwordErrorMessage}</div>
-        </div>
-        <button className="btn" type="submit">
-          Submit
-        </button>
-      </form>
-    </>
-  );
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const nameValue = nameRef.current.value;
-    const passwordValue = passwordRef.current.value;
-
-    if (!/@webdevsimplified\.com$/.test(nameValue)) {
-      showEmailError('Must end in @webdevsimplified.com');
-    } else if (passwordValue.length < 10) {
-      showPasswordError('The password should be minimum 10 characters long');
-    } else if (!/[a-z]/.test(passwordValue)) {
-      showPasswordError(
-        'The password should have at least one lowercase letter'
-      );
-    } else if (!/[A-Z]/.test(passwordValue)) {
-      showPasswordError(
-        'The password should have at least one uppercase letter'
-      );
-    } else if (!/[0-9]/.test(passwordValue)) {
-      showPasswordError('The password should include a number');
-    } else {
-      setEmailFormClass('form-group');
-      setPasswordFormClass('form-group');
-      alert('Success!');
-    }
-  }
-
-  function showPasswordError(message) {
-    setPasswordFormClass('form-group error');
-    setPasswordErrorMessage(message);
-  }
-
-  function showEmailError(message) {
-    setEmailFormClass('form-group error');
-    setEmailErrorMessage(message);
-  }
+  return <FormValidationState />;
 }
 
 export default App;
---
+++ react/basic-form-validation/src/FormValidationRef.jsx
@@ -0,0 +1,93 @@
+import { useRef, useState } from 'react';
+
+export function FormValidationRef() {
+  const nameRef = useRef();
+  const passwordRef = useRef();
+
+  const [passwordFormClass, setPasswordFormClass] = useState('form-group');
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  const [emailFormClass, setEmailFormClass] = useState('form-group');
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className={emailFormClass}>
+          <label htmlFor="email" className="label">
+            Email
+          </label>
+          <input
+            className="input"
+            type="email"
+            id="email"
+            ref={nameRef}
+            required
+          />
+          <div className="msg">{emailErrorMessage}</div>
+        </div>
+        <div className={passwordFormClass}>
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+          <input
+            id="password"
+            className="input"
+            type="password"
+            ref={passwordRef}
+            required
+          />
+          <div className="msg">{passwordErrorMessage}</div>
+        </div>
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+
+  function handleSubmit(event) {
+    // Prevent page from reloading on form submit
+    event.preventDefault();
+
+    // If one of fields is correct after first unsuccessful try, reset error style
+    setEmailFormClass('form-group');
+    setPasswordFormClass('form-group');
+    setPasswordErrorMessage('');
+    setEmailErrorMessage('');
+
+    const nameValue = nameRef.current.value;
+    const passwordValue = passwordRef.current.value;
+
+    if (!/@webdevsimplified\.com$/.test(nameValue)) {
+      showEmailError('Must end in @webdevsimplified.com');
+    }
+
+    if (passwordValue.length < 10) {
+      showPasswordError('The password should be minimum 10 characters long');
+    } else if (!/[a-z]/.test(passwordValue)) {
+      showPasswordError(
+        'The password should have at least one lowercase letter'
+      );
+    } else if (!/[A-Z]/.test(passwordValue)) {
+      showPasswordError(
+        'The password should have at least one uppercase letter'
+      );
+    } else if (!/[0-9]/.test(passwordValue)) {
+      showPasswordError('The password should include a number');
+    } else {
+      setEmailFormClass('form-group');
+      setPasswordFormClass('form-group');
+      alert('Success!');
+    }
+  }
+
+  function showPasswordError(message) {
+    setPasswordFormClass('form-group error');
+    setPasswordErrorMessage(message);
+  }
+
+  function showEmailError(message) {
+    setEmailFormClass('form-group error');
+    setEmailErrorMessage(message);
+  }
+}
