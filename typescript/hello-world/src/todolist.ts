ad10a7f6be4f67557bc7b0f62f6be8e058303215
Konstantin Kupriyanov <0864380@gmail.com>
2024-01-06 20:59:08 +0100

    todolist on vanilla js

---
+++ typescript/hello-world/src/todolist.ts
@@ -0,0 +1,104 @@
+// type Todo = {
+//   id: string;
+//   name: string;
+//   completed: boolean;
+// };
+// type NewTodo = Partial<Todo>;
+
+// const todoInput = document.getElementById('todo-input') as HTMLInputElement;
+// const form = document.getElementById('new-todo-form') as HTMLFormElement;
+// const list = document.getElementById('list') as HTMLUListElement;
+
+// console.log('localStorage:', localStorage.getItem('todos'));
+
+// const newTodoText: NewTodo = {};
+
+// let todos: Todo[] = JSON.parse(localStorage.getItem('todos') || '[]');
+
+// if (todos.length) {
+//   for (const todo of todos) {
+//     createTodoElement(todo);
+//   }
+// }
+
+// todoInput.addEventListener('keyup', saveOnChange);
+// form.addEventListener('submit', onSubmit);
+// list.addEventListener('click', handleTodoDelete);
+
+// function saveOnChange(e: KeyboardEvent): void {
+//   newTodoText.name = (e.target as HTMLInputElement).value;
+// }
+
+// function onSubmit(e: SubmitEvent) {
+//   e.preventDefault();
+//   if (newTodoText.name) {
+//     addTodo(newTodoText.name);
+//   }
+// }
+
+// function addTodo(text: string): void {
+//   const newTodo: Todo = {
+//     id: crypto.randomUUID(),
+//     name: text,
+//     completed: false,
+//   };
+
+//   todos.push(newTodo);
+//   createTodoElement(newTodo);
+
+//   localStorage.setItem('todos', JSON.stringify(todos));
+// }
+
+// function createTodoElement({ id, name, completed }: Todo): void {
+//   const li = document.createElement('li');
+//   const label = document.createElement('label');
+//   const input = document.createElement('input');
+//   const span = document.createElement('span');
+//   const button = document.createElement('button');
+
+//   button.innerText = 'Delete';
+//   input.type = 'checkbox';
+//   input.checked = completed;
+//   span.innerText = name;
+
+//   li.className = 'list-item';
+//   li.id = id;
+//   label.className = 'list-item-label';
+//   input.className = 'label-input';
+//   button.className = 'delete-btn';
+//   span.className = 'label-text';
+
+//   label.append(input);
+//   label.append(span);
+//   li.append(label);
+//   li.append(button);
+//   list.append(li);
+//   todoInput.value = '';
+// }
+
+// function handleTodoDelete(e: MouseEvent) {
+//   console.log('click');
+
+//   if (!(e.target instanceof Element)) {
+//     console.log('!(e.target instanceof Element)');
+//     return;
+//   }
+
+//   const li = e.target.closest('li');
+//   const id = li?.id;
+
+//   if (e.target.className === 'delete-btn') {
+//     todos = todos.filter((todo) => todo.id !== id);
+
+//     li?.remove();
+//   }
+
+//   if (e.target.className === 'label-input') {
+//     const completedTodo = todos.find((todo) => todo.id === id);
+//     if (completedTodo) {
+//       completedTodo.completed = !completedTodo?.completed;
+//     }
+//   }
+
+//   localStorage.setItem('todos', JSON.stringify(todos));
+// }
